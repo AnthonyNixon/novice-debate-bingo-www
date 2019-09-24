d2044bd20218378846808561a34830f75382efa1
Anthony Nixon <ajnixon@spscommerce.com>
2019-09-24 14:31:13 -0500

    update to new model

--- index.js
+++ index.js
@@ -1,119 +1,136 @@
-var app = angular.module('noviceDebateBingo', ['ngMaterial', 'ngMessages']);
-app.controller('bingoCtrl', function($scope, $http, $interval) {
-  $scope.fields = [];
-  $scope.bingo = false;
-   $http({
+var app = angular.module('noviceDebateBingo', ['ngMaterial', 'ngMessages', 'ngRoute'])
+    .config(function ($mdThemingProvider, $locationProvider, $routeProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('indigo')
+            .accentPalette('red');
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'templates/home.html'
+            })
+            .when('/:code', {
+                templateUrl: 'templates/board.html'
+            });
+    });
+
+app.controller('welcomeCtrl', function ($scope, $timeout, $http, $location) {
+    $scope.boardCode = "";
+    $scope.buttonText = "New Board";
+
+    var changeTimeout = $timeout(function() {
+        if ($scope.boardCode !== "") {
+            $scope.buttonText = "Go to " + $scope.boardCode;
+        } else {
+            $scope.buttonText = "New Board";
+        }
+    }, 100);
+
+    $scope.$watch('boardCode', function () {
+        changeTimeout.cancel;
+        changeTimeout = $timeout(function() {
+            if ($scope.boardCode !== "") {
+                $scope.buttonText = "Go to " + $scope.boardCode;
+            } else {
+                $scope.buttonText = "New Board";
+            }
+        }, 1000);
+    }, true);
+
+    $scope.goToBoard = goToBoard;
+
+    function goToBoard() {
+        if ($scope.boardCode === "") {
+            var onSuccess = function(response) {
+                var newBoardID = response.data.board.code;
+                console.log(response);
+                $location.path('/' + newBoardID);
+            };
+
+            var onError = function(data) {
+                console.error("Error creating new board.", data);
+            };
+
+            $http.post('https://api.novicedebatebingo.com/boards', {division: "novice"})
+                .then(onSuccess, onError)
+        } else {
+            $location.path('/' + $scope.boardCode);
+        }
+    }
+});
+
+app.controller('homeCtrl', function ($scope) {
+
+});
+
+app.controller('bingoCtrl', function ($scope, $http, $routeParams) {
+    $scope.fields = [];
+    $scope.bingo = false;
+    $scope.boardCode = $routeParams.code;
+
+    console.log("Boardcode", $scope.boardCode);
+
+    $http({
         method: 'GET',
-        url: 'bingo.json'
+        url: 'https://api.novicedebatebingo.com/boards/' + $scope.boardCode
     }).then(function successCallback(response) {
         data = response.data;
-        console.log(data);
-
-        for (i = 0; i < data.fields.length; i++) {
-            console.log(i);
-            data.fields[i].checked = false;
-            $scope.fields.push(data.fields[i]);
-        }
-        
-        $scope.fields = shuffle($scope.fields);
-        
-        $scope.fields.splice(12, 0, ({"content": "Free Space!", "checked": true}));
-        $scope.grid = new Array(5)
-        for (x = 0; x < 5; x++) {
-          $scope.grid[x] = new Array(5);
-        }
-
-        
-        for (y = 0; y < 5; y++) {
-          for (x = 0; x < 5; x++) {
-            $scope.grid[x][y] = $scope.fields[(y*5) + x];
-          }
-        }
-
-        $scope.grid[2][2].checked = true;
-        console.log("hi");
-        console.log($scope.fields);
-        console.log($scope.grid);
+        updateGrid(data)
 
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
     });
-    
-  function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+
+    function clickCell(x, y) {
+        console.log("Clicking", x, y);
+        var onSuccess = function(response) {
+            updateGrid(response.data);
+        };
+
+        var onError = function(data) {
+            console.error("Error posting toggle event", data);
+        };
+
+        $http.post('https://api.novicedebatebingo.com/toggleBox', { board_id: $scope.boardCode, coordinates: {x: x, y: y}})
+            .then(onSuccess, onError)
+
     }
-  
-    return array;
-  }
-   
-  $scope.$watch('grid', function() {
-    $scope.bingo = checkBingo($scope.grid);
-    console.log("$scope.bingo is ", $scope.bingo);
-    console.log("hi");
-  }, true);
-  
-  $scope.$watch('bingo', function() {
-    if ($scope.bingo) {
-       gtag('event', 'bingo', {
-            'event_category': 'bingo',
-            'event_label': 'bingo'
-        });
+
+    function updateGrid(data) {
+        console.log(data);
+
+        $scope.grid = new Array(5);
+        for (x = 0; x < 5; x++) {
+            $scope.grid[x] = new Array(5);
+        }
+
+        for (x = 0; x < 5; x++) {
+            for (y = 0; y < 5; y++) {
+                $scope.grid[x][y] = {content: data.boxes[x][y].content, checked: data.boxes[x][y].checked, coordinates: data.boxes[x][y].coordinates};
+            }
+        }
+
+        console.log("hi");
+        console.log($scope.grid);
     }
-    console.log("BINGO! ", $scope.bingo);
-  })
-  
-  function checkBingo(array) {
-    console.log("Checking bingos.");
-    console.log(array);
-    //Check Horizontal Bingos
-    if (array[0][0].checked && array[0][1].checked && array[0][2].checked && array[0][3].checked && array[0][4].checked) {
-      return true;
-    } else if (array[1][0].checked && array[1][1].checked && array[1][2].checked && array[1][3].checked && array[1][4].checked) {
-      return true;
-    } else if (array[2][0].checked && array[2][1].checked && array[2][2].checked && array[2][3].checked && array[2][4].checked) {
-      return true;
-    } else if (array[3][0].checked && array[3][1].checked && array[3][2].checked && array[3][3].checked && array[3][4].checked) {
-      return true;
-    } else if (array[4][0].checked && array[4][1].checked && array[4][2].checked && array[4][3].checked && array[4][4].checked) {
-      return true;
-    }
-    
-    // Check Horizontal Bingos
-    else if (array[0][0].checked && array[1][0].checked && array[2][0].checked && array[3][0].checked && array[4][0].checked) {
-      return true;
-    } else if (array[0][1].checked && array[1][1].checked && array[2][1].checked && array[3][1].checked && array[4][1].checked) {
-      return true;
-    } else if (array[0][2].checked && array[1][2].checked && array[2][2].checked && array[3][2].checked && array[4][2].checked) {
-      return true;
-    } else if (array[0][3].checked && array[1][3].checked && array[2][3].checked && array[3][3].checked && array[4][3].checked) {
-      return true;
-    } else if (array[0][4].checked && array[1][4].checked && array[2][4].checked && array[3][4].checked && array[4][4].checked) {
-      return true;
-    }
-    
-    // Check Diagonal Bingos
-    else if (array[0][0].checked && array[1][1].checked && array[2][2].checked && array[3][3].checked && array[4][4].checked) {
-      return true;
-    } else if (array[4][0].checked && array[3][1].checked && array[2][2].checked && array[1][3].checked && array[0][4].checked) {
-      return true;
-    } else {
-      return false;
-    }
-    
-  }
+
+    $scope.clickCell = clickCell
+
+    // $scope.$watch('grid', function () {
+    //     $scope.bingo = checkBingo($scope.grid);
+    //     console.log("$scope.bingo is ", $scope.bingo);
+    //     console.log("hi");
+    // }, true);
+
+    // $scope.$watch('bingo', function () {
+    //     if ($scope.bingo) {
+    //         gtag('event', 'bingo', {
+    //             'event_category': 'bingo',
+    //             'event_label': 'bingo'
+    //         });
+    //     }
+    //     console.log("BINGO! ", $scope.bingo);
+    // })
 });
 
 
